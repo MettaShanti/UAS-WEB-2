0c351084761a38680f861488694cbf5a349c3426
MettaShanti <mettashanti_2225110011@mhs.mdp.ac.id>
2025-01-08 11:21:28 +0700

    -

--- react-crud/src/components/Home.jsx
+++ react-crud/src/components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home(){
     return(
         <>
             <h2>Home</h2>
-            <h2>Selamat Datang di React CRUD</h2>
+            <h2>Selamat Datang di React CRUD Iventory Barang</h2>
         </>
     )
 }
