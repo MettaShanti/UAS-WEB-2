2aa3a9583874067a5b3c5601ebdc6ca1b47ed992
MettaShanti <mettashanti_2225110011@mhs.mdp.ac.id>
2025-01-10 11:43:30 +0700

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,26 +10,26 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
 
-public function login(Request $request)
-{
-    if(Auth::attempt([
-        'email' => $request->email,
-        'password' => $request->password
-    ])) {
-        $user = Auth::user(); // ambil data user dari table users sesuai dengan email  dan pass
-
-        //untuk pemberian hak akses
-        if($user->role == 'admin'){
-            $success['token'] = $user->createToken('MDPApp',['create','read','update','delete'])->plainTextToken; // buat token
-        }else{
-            $success['token'] = $user->createToken('MDPApp',['read'])->plainTextToken; // buat token
+    public function login(Request $request)
+    {
+        if(Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            $user = Auth::user(); // ambil data user dari table users sesuai dengan email  dan pass
+    
+            //untuk pemberian hak akses
+            if($user->role == 'admin'){
+                $success['token'] = $user->createToken('MDPApp',['create','read','update','delete'])->plainTextToken; // buat token
+            }else{
+                $success['token'] = $user->createToken('MDPApp',['read'])->plainTextToken; // buat token
+            }
+    
+            $success['token'] = $user->createToken('MDPApp')->plainTextToken; // buat token
+            $success['name'] = $user->name;// response nama user
+            return response()->json($success, 201);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
-
-        $success['token'] = $user->createToken('MDPApp')->plainTextToken; // buat token
-        $success['name'] = $user->name;// response nama user
-        return response()->json($success, 201);
-    } else {
-        return response()->json(['error' => 'Unauthorized'], 401);
     }
-}
-}
+    }
