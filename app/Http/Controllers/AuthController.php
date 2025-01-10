7304b8e513df461d84c733c14aa179dc58572c1b
MettaShanti <mettashanti_2225110011@mhs.mdp.ac.id>
2025-01-10 12:26:42 +0700

    login pakai token berhasil

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,33 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-
     public function login(Request $request)
-    {
+{
         if(Auth::attempt([
             'email' => $request->email,
             'password' => $request->password
         ])) {
-            $user = Auth::user(); // ambil data user dari table users sesuai dengan email  dan pass
-    
-            //untuk pemberian hak akses
+            $user = Auth::user();//ambil data user dr tabel users sesuai dgn email dan pass
             if($user->role == 'admin'){
-                $success['token'] = $user->createToken('MDPApp',['create','read','update','delete'])->plainTextToken; // buat token
-            }else{
-                $success['token'] = $user->createToken('MDPApp',['read'])->plainTextToken; // buat token
+                $success['token'] = $user->createToken('MDPApp',
+                ['create', 'read', 'update', 'delete']) ->plainTextToken;//buat token            
+            }else {
+                $success['token'] = $user->createToken('MDPApp'
+                ['read'])->plainTextToken;//buat token               
             }
-    
-            $success['token'] = $user->createToken('MDPApp')->plainTextToken; // buat token
-            $success['name'] = $user->name;// response nama user
+
+            $success['token'] = $user->createToken('MDPApp')->plainTextToken;
+            $success['name'] = $user->name;
             return response()->json($success, 201);
         } else {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
     }
-    }
+}
